2cb8e872bd4c81c36f63d24651cffb9b2c0f5af2
Kathrine <61585423+Kat1606@users.noreply.github.com>
2021-04-07 11:30:14 +0200

    Added classes

---
+++ BirthClinicPlanning/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthClinicPlanning.Models
+{
+    public class Employee
+    {
+        public string Title { get; set; } //Secretarie og clinician
+        public string FullName { get; set; }
+    }
+}
