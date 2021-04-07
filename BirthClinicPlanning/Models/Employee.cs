c22fd3e6cb47194a529ff4c0983c2dc43027d558
Kathrine <61585423+Kat1606@users.noreply.github.com>
2021-04-07 11:32:54 +0200

    added employee id

--- BirthClinicPlanning/Models/Employee.cs
+++ BirthClinicPlanning/Models/Employee.cs
@@ -8,6 +8,7 @@ namespace BirthClinicPlanning.Models
 {
     public class Employee
     {
+        public int EmployeeId { get; set; }
         public string Title { get; set; } //Secretarie og clinician
         public string FullName { get; set; }
     }
