8d29403ac9fefb3493b219fc500092a0f7893033
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-04-14 22:29:28 +0200

    added projekt

--- BirthClinicPlanningConsole/Program.cs
+++ BirthClinicPlanningConsole/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Reflection.Metadata;
-using System.Reflection.PortableExecutable;
 using BirthClinicPlanning.Data;
 using BirthClinicPlanning.Models;
 
